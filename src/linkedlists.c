f2bd78bfedbb0b7a1047cdfdebd5ff6d766869b7
고태양 <113888279+KTaey129@users.noreply.github.com>
2024-10-09 23:34:11 +0000

    this is commit

--- src/linkedlists.c
+++ src/linkedlists.c
@@ -1,27 +1,43 @@
 #include <stddef.h>
 
-struct IntElement {
-    struct IntElement* next;
-    int data;
+// struct IntElement {
+//     struct IntElement* next;
+//     int data;
+// };
+
+// void IntElement_init(struct IntElement* elem, int value) {
+//     elem->next = NULL;
+//     elem->data = value;
+// }
+
+// struct IntElement* IntElement_getNext(struct IntElement* elem) {
+//     return elem->next;
+// }
+
+// int IntElement_value(struct IntElement* elem) {
+//     return elem->data;
+// }
+
+// void IntElement_setNext(struct IntElement* elem, struct IntElement* nextElem) {
+//     elem->next = nextElem;
+// }
+
+// void IntElement_setValue(struct IntElement* elem, int value) {
+//     elem->data = value;
+// }
+
+// A singly linked list in C++
+class IntElement {
+    public:
+        IntElement(int value) : next(NULL), data(value) {}
+        ~tElement() {}
+
+        IntElement* getNext() const { return next; }
+        int value() const { return data; }
+        void setNext(IntElement* nextElem) { next = nextElem; }
+        void setValue(int value) { data = value; }
+        
+    private:
+        IntElement* next;
+        int data;
 };
-
-void IntElement_init(struct IntElement* elem, int value) {
-    elem->next = NULL;
-    elem->data = value;
-}
-
-struct IntElement* IntElement_getNext(struct IntElement* elem) {
-    return elem->next;
-}
-
-int IntElement_value(struct IntElement* elem) {
-    return elem->data;
-}
-
-void IntElement_setNext(struct IntElement* elem, struct IntElement* nextElem) {
-    elem->next = nextElem;
-}
-
-void IntElement_setValue(struct IntElement* elem, int value) {
-    elem->data = value;
-}
